00bf5efe8533f6c91e43ed0058c9f062f131d5b9
JAYD3V <W3Dojo@Gmail.com>
2022-04-17 04:08:26 -0400

    Added regular expression for preforming a numeric check on a string.

---
+++ RegX/is-whole-num.js
@@ -0,0 +1,18 @@
+/** 
+  * @file "is-whole-num.js"
+  *
+  * This file contains a function that tests a string using a regular expression. 
+  * The test only works for whole numbers. Like any JavaScript number, there is a 
+  * limit to the size that the number can be. 
+  *
+  * Why not use one of the built-in methods for preforming the same type of check?
+  * Regular expressions are much faster in specific situations than program
+  * implimented algorithms; checking whether or not a string is a whole number is 
+  * one of those situations. Thats not to say that regular expressions should be 
+  * used in every instance of checking whether or not a string is numeric, 
+  * obviously, the choice you make (algorithm or regex) should vary on a case by 
+  * case basis.
+  */
+function isNumeric(value) {
+    return /^-?\d+$/.test(value);
+}
